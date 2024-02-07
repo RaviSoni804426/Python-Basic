d11b377bf0ea97629b5f3ad2d1c33ecd1f54cefb
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-02-07 16:27:00 +0530

    Add files via upload

---
+++ Strings/class.py
@@ -0,0 +1,8 @@
+class student:
+    def set_name(self,name):
+        self.name=name
+    def get_name(self):
+        return self.name
+student1=student()
+student1.set_name("Ravi") 
+print(student1.name())
